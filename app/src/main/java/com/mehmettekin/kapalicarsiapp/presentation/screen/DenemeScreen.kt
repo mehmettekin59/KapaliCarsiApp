1a7efe0c9d712d20584149a3bd856bd8fb271fa0
mehmettekin59 <mehmettekin59@gmail.com>
2025-04-02 20:26:21 +0300

    deneme screen eklendi

---
+++ app/src/main/java/com/mehmettekin/kapalicarsiapp/presentation/screen/DenemeScreen.kt
@@ -0,0 +1,26 @@
+package com.mehmettekin.kapalicarsiapp.presentation.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun DenemeScreen(
+    viewModel: KapaliCarsiViewModel = hiltViewModel()
+) {
+    val exchangeRatesState by viewModel.exchangeRates.collectAsState()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
